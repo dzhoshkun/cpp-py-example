37712b40a7c045be897febc90e47489015d1f187
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2017-03-20 15:38:37 +0200

    Issue #6: added spaces for clarity

--- ex/basic.py
+++ ex/basic.py
@@ -3,4 +3,4 @@ from pymycpp import Cimage
 
 if __name__ == '__main__':
     img = Cimage(1920, 1080)
-    print('{} x {} image'.format(img.width(), img.height()))
+    print( '{} x {} image'.format( img.width(), img.height() ) )
