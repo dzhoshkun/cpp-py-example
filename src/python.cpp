52991acfffd95b52d4f334ac5f8fe2002836f79d
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-20 13:14:58 +0000

    Issue #6: exposed Cimage to python

--- src/python.cpp
+++ src/python.cpp
@@ -1,4 +1,14 @@
-int main()
+#include <boost/python.hpp>
+#include "cimage.h"
+
+
+using namespace boost::python;
+
+
+BOOST_PYTHON_MODULE(pymycpp)
 {
-    return 0;
+    class_<Cimage>("Cimage", init<size_t, size_t>())
+        .def("width", &Cimage::width)
+        .def("height", &Cimage::height)
+        ;
 }
