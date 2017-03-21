08f413278708c10cea2ca647dfb8713760144562
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-21 09:30:44 +0000

    Issue #1: added sleep to Cproc run

--- src/cproc.cpp
+++ src/cproc.cpp
@@ -1,10 +1,12 @@
 #include <thread>
+#include <chrono>
 #include "cproc.h"
 
 
 void Cproc::run(Cimage & image)
 {
     std::thread thr(&Cproc::mt_run, this, std::ref(image));
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
     thr.join();
 }
 
