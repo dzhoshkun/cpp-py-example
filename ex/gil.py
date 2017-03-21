233ef62fb4428d09a4595b1ab67667102755de92
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-21 09:53:44 +0000

    Issue #1: GIL example sleeping

--- ex/gil.py
+++ ex/gil.py
@@ -1,5 +1,6 @@
 from pymycpp import Cimage, Cproc
 from inheritance import Pimage
+from time import sleep
 
 
 if __name__ == '__main__':
@@ -7,4 +8,8 @@ if __name__ == '__main__':
     cimg = Cimage(500, 300)
     pimg = Pimage(750, 450)
     proc.run(cimg)
+    sleep(5)
+    proc.stop()
     proc.run(pimg)
+    sleep(5)
+    proc.stop()
