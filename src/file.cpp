601054fe6d56b7dd310975cfaa089decc1d50097
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-22 16:14:37 +0000

    Issue #9: implemented File read

--- src/file.cpp
+++ src/file.cpp
@@ -1,14 +1,29 @@
 #include "file.h"
+#include "except.h"
 
+
+unsigned char bmp_header[54];
 
 Cimage * File::read(std::string filepath)
 {
-    // TODO
-    return nullptr;
+    FILE * fptr = fopen(filepath.c_str(), "rb");
+    if (fptr == nullptr)
+        throw FileNotFound();
+
+    fread(bmp_header, sizeof(unsigned char), 54, fptr);
+    int width = *(int *)&bmp_header[18];
+    int height = *(int *)&bmp_header[22];
+
+    size_t num_bytes = 3 * width * height;
+    unsigned char * data = new unsigned char[num_bytes];
+    fread(data, sizeof(unsigned char), num_bytes, fptr);
+
+    fclose(fptr);
+
+    return new Cimage(data, width, height);
 }
 
 void File::write(const Cimage & image, std::string filepath)
 {
     // TODO
 }
-    // TODO
