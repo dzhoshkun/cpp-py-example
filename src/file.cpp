da1e101afd20f309bbfedec4c80da940514bb73b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-22 14:36:08 +0000

    Issue #9: added stubs for File class

---
+++ src/file.cpp
@@ -0,0 +1,14 @@
+#include "file.h"
+
+
+Cimage * File::read(std::string filepath)
+{
+    // TODO
+    return nullptr;
+}
+
+void File::write(const Cimage & image, std::string filepath)
+{
+    // TODO
+}
+    // TODO
