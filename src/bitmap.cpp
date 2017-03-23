5d37695956252cbde37ee31bc81a273629d4895a
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-23 16:53:29 +0000

    Issue #14: added stubs for Bitmap class

---
+++ src/bitmap.cpp
@@ -0,0 +1,36 @@
+#include "bitmap.h"
+
+
+Bitmap::Bitmap(std::string filepath)
+{
+    // TODO
+}
+
+Bitmap::~Bitmap()
+{
+    // TODO
+}
+
+uint8_t * const Bitmap::get_data() const
+{
+    // TODO
+    return nullptr;
+}
+
+size_t Bitmap::get_width() const
+{
+    // TODO
+    return 0;
+}
+
+size_t Bitmap::get_height() const
+{
+    // TODO
+    return 0;
+}
+
+std::string Bitmap::save() const
+{
+    // TODO
+    return "";
+}
---
+++ src/bitmap.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <string>
+
+
+class Bitmap
+{
+    protected:
+        uint8_t * _data;
+        size_t _width;
+        size_t _height;
+
+    public:
+        Bitmap(std::string filepath);
+        virtual ~Bitmap();
+
+    public:
+        uint8_t * const get_data() const;
+        size_t get_width() const;
+        size_t get_height() const;
+        std::string save() const;
+};
