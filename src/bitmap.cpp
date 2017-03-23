d8fa5cae72b365534d77de4ac9baa4ca12ba6bd9
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-23 19:42:24 +0000

    Issue #14: now adding a count to timestamp as well for uniqueness

--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -3,18 +3,19 @@
 #include <ctime>
 
 
-std::string timestamp(std::string filepath)
+std::string Bitmap::timestamp(std::string filepath)
 {
     std::time_t now = std::time(0);
     std::tm * ltm = std::localtime(&now);
     char ts[50];
-    sprintf(ts, "-%d-%02d-%02d-%02d-%02d-%02d",
+    sprintf(ts, "-%d-%02d-%02d-%02d-%02d-%02d-%03lu",
             1900 + ltm->tm_year,
             1 + ltm->tm_mon,
             ltm->tm_mday,
             1 + ltm->tm_hour,
             1 + ltm->tm_min,
-            1 + ltm->tm_sec);
+            1 + ltm->tm_sec,
+            ++_ctr);
     size_t pos = filepath.find(".bmp");
     std::string filepath_ts = filepath.insert(pos, ts);
     return filepath_ts;
@@ -25,6 +26,7 @@ Bitmap::Bitmap(std::string filepath)
       , _data(nullptr)
       , _width(0)
       , _height(0)
+      , _ctr(0)
 {
     FILE * fptr = fopen(_filepath.c_str(), "rb");
     if (fptr == nullptr)
@@ -64,7 +66,7 @@ size_t Bitmap::get_height() const
     return _height;
 }
 
-std::string Bitmap::save() const
+std::string Bitmap::save()
 {
     std::string filepath_ts = timestamp(_filepath);
     FILE * fptr = fopen(filepath_ts.c_str(), "wb");
--- src/bitmap.h
+++ src/bitmap.h
@@ -8,6 +8,7 @@ class Bitmap
 {
     protected:
         std::string _filepath;
+        size_t _ctr;
         uint8_t * _data;
         size_t _width;
         size_t _height;
@@ -20,5 +21,8 @@ class Bitmap
         uint8_t * const get_data() const;
         size_t get_width() const;
         size_t get_height() const;
-        std::string save() const;
+        std::string save();
+
+    protected:
+        std::string timestamp(std::string filepath);
 };
