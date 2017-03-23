905ba77c3a015a9f0ea4f6ea9f98460d29963b57
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-23 17:22:07 +0000

    Issue #14: implemented save method

--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -2,6 +2,8 @@
 #include "except.h"
 
 
+std::string timestamp(std::string filepath);
+
 Bitmap::Bitmap(std::string filepath)
     : _filepath(filepath)
       , _data(nullptr)
@@ -47,6 +49,38 @@ size_t Bitmap::get_height() const
 }
 
 std::string Bitmap::save() const
+{
+    std::string filepath_ts = timestamp(_filepath);
+    FILE * fptr = fopen(filepath_ts.c_str(), "wb");
+    if (fptr == nullptr)
+        throw FileError();
+
+    unsigned char bmp_header[54] = {'B','M',
+                                    0, 0, 0, 0,
+                                    0, 0,
+                                    0, 0,
+                                    54, 0, 0, 0,
+                                    40, 0, 0, 0,
+                                    0, 0, 0, 0,
+                                    0, 0, 0, 0,
+                                    1, 0,
+                                    24, 0};
+
+    size_t num_bytes = 3 * _width * _height;
+    *(int *)&bmp_header[2] = (int)(54 + num_bytes);
+    *(int *)&bmp_header[18] = (int) _width;
+    *(int *)&bmp_header[22] = (int) _height;
+
+    fwrite(bmp_header, sizeof(uint8_t), 54, fptr);
+
+    fwrite(_data, sizeof(uint8_t), num_bytes, fptr);
+
+    fclose(fptr);
+
+    return filepath_ts;
+}
+
+std::string timestamp(std::string filepath)
 {
     // TODO
     return "";
