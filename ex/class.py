3b0ddddaf1721f3ee58afbb60b2df42c8efc9038
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-25 14:03:34 +0000

    Simplified class.py code

--- ex/class.py
+++ ex/class.py
@@ -4,9 +4,13 @@ from pymycpp import Bitmap
 filepath = 'baboon.bmp'
 
 img = Bitmap(filepath)
-print( '{}x{} image read'
-       ' from {}'.format( img.get_width(),
-                          img.get_height(),
-                          filepath ) )
 
-print( 'Saving image to {}'.format(img.save()) )
+width = img.get_width()
+height = img.get_height()
+
+print( '{}x{} image read from {}'.format(
+    width, height, filepath ) )
+
+new_filepath = img.save()
+print( 'Saving image to {}'.format(
+    new_filepath ) )
