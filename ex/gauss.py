09751975022de4f5832027178fb370587133b10b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-25 12:29:10 +0000

    Issue #19: added Gaussian smoothing using Scipy

---
+++ ex/gauss.py
@@ -0,0 +1,15 @@
+from pymycpp import Bitmap
+import scipy.ndimage as ndi
+
+
+if __name__ == '__main__':
+    img = Bitmap('baboon.bmp')
+
+    data_np = img.data()
+
+    ndi.gaussian_filter(input=data_np,
+                        sigma=(5, 15, 0),
+                        order=0,
+                        output=data_np)
+
+    img.save()
