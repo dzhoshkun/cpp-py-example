985584dc85104992ea4960a98f9d67d5ed42e963
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-28 09:03:26 +0100

    removed extra newlines from class.py

--- ex/class.py
+++ ex/class.py
@@ -11,9 +11,7 @@ if __name__ == '__main__':
     width = img.get_width()
     height = img.get_height()
 
-    print( '{}x{} image read from {}'.format(
-        width, height, filepath ) )
+    print( '{}x{} image read from {}'.format( width, height, filepath ) )
 
     new_filepath = img.save()
-    print( 'Saving image to {}'.format(
-        new_filepath ) )
+    print( 'Saving image to {}'.format( new_filepath ) )
