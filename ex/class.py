112500549a77ca1223a7c33811a87cf5f6bc9792
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-03-28 09:02:13 +0100

    class.py now uses main function

--- ex/class.py
+++ ex/class.py
@@ -3,16 +3,17 @@
 from pymycpp import Bitmap
 
 
-filepath = 'baboon.bmp'
+if __name__ == '__main__':
+    filepath = 'baboon.bmp'
 
-img = Bitmap(filepath)
+    img = Bitmap(filepath)
 
-width = img.get_width()
-height = img.get_height()
+    width = img.get_width()
+    height = img.get_height()
 
-print( '{}x{} image read from {}'.format(
-    width, height, filepath ) )
+    print( '{}x{} image read from {}'.format(
+        width, height, filepath ) )
 
-new_filepath = img.save()
-print( 'Saving image to {}'.format(
-    new_filepath ) )
+    new_filepath = img.save()
+    print( 'Saving image to {}'.format(
+        new_filepath ) )
