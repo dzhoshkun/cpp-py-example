27fbb2b5a6fa70fe63ae426a07535b47d8ac6124
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2017-03-28 09:07:59 +0100

    Only direct Bitmap exposure in python.cpp

--- src/python.cpp
+++ src/python.cpp
@@ -1,66 +1,12 @@
 #include <boost/python.hpp>
-#include <boost/python/exception_translator.hpp>
-#include "cimage.h"
-#include "cproc.h"
-#include "gil.h"
-#include "except.h"
 #include "bitmap.h"
 
 
 using namespace boost::python;
 
 
-class CimageWrapper
-    : public Cimage
-    , public wrapper<Cimage>
-{
-public:
-    CimageWrapper(size_t width, size_t height)
-        : Cimage(width, height)
-    {
-
-    }
-
-public:
-    void info()
-    {
-        ScopedPythonGILLock gil_lock;
-        override f = this->get_override("info");
-        if (f)
-            f();
-        else
-            Cimage::info();
-    }
-};
-
-
-void translate_FileError(FileError const & e)
-{
-    PyErr_SetString(PyExc_OSError, "FileError");
-}
-
-
 BOOST_PYTHON_MODULE(pymycpp)
 {
-    PyEval_InitThreads();
-
-    register_exception_translator<FileError>(&translate_FileError);
-
-    class_<CimageWrapper, boost::noncopyable>(
-            "Cimage", init<size_t, size_t>())
-        .def("width", &CimageWrapper::width)
-        .def("height", &CimageWrapper::height)
-        .def("how_many_bytes", &CimageWrapper::how_many_bytes)
-        .staticmethod("how_many_bytes")
-        .def("info", &CimageWrapper::info)
-        ;
-
-    class_<Cproc, boost::noncopyable>(
-            "Cproc", init<>())
-        .def("run", &Cproc::run)
-        .def("stop", &Cproc::stop)
-        ;
-
     class_<Bitmap, boost::noncopyable>(
             "Bitmap", init<std::string>())
         .def("get_width", &Bitmap::get_width)
